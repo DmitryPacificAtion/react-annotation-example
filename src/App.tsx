034d33b014dcb6b37c4bd764919f18898e4cf4dd
Dmitry Bruslik <dmitrii.bruslik@ignited.it>
2020-12-03 22:56:42 +0200

    Use Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
-import { Header } from './components'
+import { Header, Canvas } from './components';
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Canvas />
       <footer>
         To leave a comment, mouseover <i>cross</i> on an image and click the
         left mouse button <i>mouse</i>
--- src/components/Header/UploadButton/index.tsx
+++ src/components/Header/UploadButton/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { FC } from 'react';
 import css from './styles.module.scss';
 
-export const UploadButton = () => {
+export const UploadButton: FC = () => {
   const { container, input, uploadButton } = css;
   return (
     <form className={container}>
@@ -10,5 +10,5 @@ export const UploadButton = () => {
       <input className={input} type="file" />
       </label>
     </form>
-  )
-}
+  );
+};
